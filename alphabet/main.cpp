ef48005cb71875f837ae6fca8c1b7bbe2001f947
KarpovNikolay <90675079+KarpovNikolay@users.noreply.github.com>
2021-10-19 09:54:14 +0300

    pochinka z

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -41,51 +41,7 @@ screen mainscreen;
 
 int main()
 {
-/**
-1)Äåëàåì áóêâó â áëîêíîòå èëè ãäåòî åù¸
-Ïðèìåð:
-Áóêâà À
-We make a letter in a notebook or somewhere else
-Example:
-Letter A
-/---\
-|   |
-|---|
-|   |
-|   |
-Îáÿçàòåëüíî òðåáóåòñÿ , ÷òîáû âñå ñòðî÷êè áûëè îäíîé äëèííû , äàæå åñëè ïðèä¸òñÿ ïðîñòî ïîñòàâèòü ïðîáåëû
-It is absolutely required that all lines are of the same length , even if you just have to put spaces
-2)Óäâàèâàåì \ åñëì îíè åñòü
-Double \ if there are any
-/---\\
-|   |
-|---|
-|   |
-|   |
-3)Ñîçäà¸ì ñèìâîë , íàçûâàåì íóæíîé áóêâîé , â ñêîáî÷êè ïèøåì êàæäóþ ñòðîêó ÷åðåç çàïÿòóþ â êàâû÷êàõ
-We create a symbol , call it the desired letter , write each line in parentheses separated by commas in quotation marks
-symbol a("/---\\","|   |","|---|","|   |","|   |");
-
-4)Ïðîâåðÿåì âûâîä
-Checking the output
-mainscreen.add(a);
-mainscreen.add(a);
-mainscreen.print();
-Åñëè âûâåëîñü
-If you have
-/---\/---\
-|   ||   |
-|---||---|
-|   ||   |
-|   ||   |
-, òî îòëè÷íî.
-Óäàëÿåì âñ¸ , ÷òî íàïèñàëè â 4 ïóíêòå è êàìèòèì â ñìàðòãèòå ñ ñîîòâåòñòâóþùèì íàçâàíèåì
-that's great.
-We delete everything that was written in paragraph 4 and add it to the smartgit with the appropriate name
-5)Ïóëèì íà ãèòõàá
-Push on github
-**/
-symbol z("----/","   / ","  /  "," /   ","/----");
+symbol z("____","   /","  / "," /  ","/___");
 symbol a("/---\\","|   |","|---|","|   |","|   |");
     return 0;
 }
